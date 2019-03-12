4c318fb44428121ea86515253f49db01fe832ae1
Ihor <i.p.kryvosheia@gmail.com>
2019-03-12 16:25:36 +0200

    initial commit

---
+++ app/src/main/java/com/st0rmtroop3r/telegramchart/MainActivity.java
@@ -0,0 +1,14 @@
+package com.st0rmtroop3r.telegramchart;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
